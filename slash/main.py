7b2fe45556f04e93f072fd407f23a64b932635f3
lavirlifiliol <adam.hrbac@gmail.com>
2021-07-23 17:15:45 +0200

    Implement a basic interaction

--- slash/main.py
+++ slash/main.py
@@ -1,15 +1,26 @@
 from pathlib import Path
 import os
 
-from flask import Flask
+from flask import Flask, request, jsonify
 from deta import App
 from upload_commands import build_commands
+from discord_interactions import verify_key_decorator, InteractionType, InteractionResponseType
+
 app = App(Flask(__name__))
 
 
-@app.route("/", methods=["GET"])
-def hello_world():
-    return "hello, world"
+@app.route("/", methods=["POST"])
+@verify_key_decorator(os.environ['CLIENT_PUBLIC'])
+def interact():
+    if request.json['type'] == InteractionType.APPLICATION_COMMAND:
+
+        data = request.json['data']
+        return jsonify({
+            'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            'data': {
+                'content': 'hello world' if data['options'][0]['name'] == 'up' else 'hello there'
+            }
+        })
 
 @app.lib.run(action='test')
 def test(ev):
